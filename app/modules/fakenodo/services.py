3e1e58604a7aa204cdc6f88dba2d9a8bb50494ff
Ignacio Blanquero Blanco <ignacio.blanquero@gmail.com>
2024-11-24 10:44:24 +0100

    feat: Fakenodoservices creation
    Creating services for Fakenodo #21

---
+++ app/modules/fakenodo/services.py
@@ -0,0 +1,6 @@
+from app.modules.fakenodo.repositories import FakenodoRepository
+from core.services.BaseService import BaseService
+
+class FakenodoService(BaseService):
+    def __init__(self):
+        super().__init__(FakenodoRepository())
