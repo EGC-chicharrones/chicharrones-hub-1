eda7d8fc20c4de521f52ad7295ffc285368c15bc
Ignacio Blanquero Blanco <ignacio.blanquero@gmail.com>
2024-11-24 10:39:29 +0100

    feat: Fakenodo repository creation
    Setting up Fakenodo repository #21
    
    Co-authored-by: vDavidd <vDavidd@users.noreply.github.com>

---
+++ app/modules/fakenodo/repositories.py
@@ -0,0 +1,6 @@
+from app.modules.fakenodo.models import Fakenodo
+from core.repositories.BaseRepository import BaseRepository
+
+class FakenodoRepository(BaseRepository):
+    def __init__(self):
+        super().__init__(Fakenodo)
