c1bff9e07a5133cfb195b5d500a65ed8c9c749d4
David Vargas Jorba <123vargasdavid@gmail.com>
2024-11-26 13:06:59 +0100

    fix: Added seeders for downloading one datase.
    We needed to add seeders in dataset, auth, featuremodel and hubfile in order to download a dataset in one format.
    Co-authored-by: Ignacio Blanquero <ignblabla@users.noreply.github.com>

--- app/modules/hubfile/seeders.py
+++ app/modules/hubfile/seeders.py
@@ -1,12 +1,20 @@
 from core.seeders.BaseSeeder import BaseSeeder
+from app import db
+from app.modules.hubfile.models import Hubfile
 
 
 class HubfileSeeder(BaseSeeder):
-
+    
+    priority = 12
     def run(self):
 
         data = [
-            # Create any Model object you want to make seed
-        ]
+            Hubfile(id=1, name="model1", checksum="123", size=12, feature_model_id=1),
+            Hubfile(id=2, name="model2", checksum="123", size=12, feature_model_id=2),
+            Hubfile(id=3, name="model3", checksum="123", size=12, feature_model_id=3),
+            Hubfile(id=4, name="model4", checksum="123", size=12, feature_model_id=4)
 
+        ]
+        
         self.seed(data)
+        db.session.commit()
