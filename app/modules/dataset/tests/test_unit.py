1bbb591d2dff2d54d5e954d40d1f26f99488f084
David Vargas Jorba <davvarjor@alum.us.es>
2024-12-06 11:04:21 +0100

    test: unit tests of download all dataset.
    Testing that datatype is a zip and the zip has files.

--- app/modules/dataset/tests/test_unit.py
+++ app/modules/dataset/tests/test_unit.py
@@ -1,9 +1,20 @@
 import pytest
 
+from app.modules.auth.models import User
 from app.modules.conftest import login
 from io import BytesIO
+import zipfile
+import pytest
+from datetime import datetime, timezone
+from io import BytesIO
+import zipfile
 
-
+from app.modules.dataset.models import DataSet
+from app.modules.featuremodel.models import FeatureModel
+from app import db
+from app.modules.hubfile.models import Hubfile
+   
+    
 @pytest.fixture(scope="module")
 def test_client(test_client):
     """
@@ -34,3 +45,14 @@ def test_upload_dataset_success(test_client):
         follow_redirects=True)
 
     assert response.status_code == 200, "Upload was successful."
+
+
+def test_download_all_datasets(test_client):
+    """
+    Test to verify that /dataset/download/all works correctly.
+    """
+    response = test_client.get("/dataset/download/all", follow_redirects=True)
+
+    assert response.status_code == 200, "The query has to be successful"
+    assert response.content_type == "application/zip", "The file type must be a ZIP."
+    assert len(response.data) > 0, "The ZIP file must not be empty." 
