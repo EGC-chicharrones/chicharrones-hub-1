d197c18f7d43e9e9bb488ead908052a6932e8f23
Ignacio Blanquero Blanco <ignacio.blanquero@gmail.com>
2024-12-12 12:25:54 +0100

    test: add locust test
    Add Locust test for dataset rating feature

--- app/modules/dataset/tests/locustfile.py
+++ app/modules/dataset/tests/locustfile.py
@@ -1,11 +1,36 @@
+from bs4 import BeautifulSoup
 from locust import HttpUser, TaskSet, task
 from core.locust.common import get_csrf_token
 from core.environment.host import get_host_for_locust_testing
 
-
-class DatasetBehavior(TaskSet):
+class CombinedBehavior(TaskSet):
     def on_start(self):
-        self.dataset()
+        self.view_rating_form()
+
+    @task
+    def view_rating_form(self):
+        dataset_id = 1
+        response = self.client.get(f"/dataset/rate/{dataset_id}/")
+        self.csrf_token = get_csrf_token(response)  # Almacenar el token CSRF en una variable de instancia
+        print(f"CSRF token obtained: {self.csrf_token}")  # Imprimir el token CSRF
+
+    @task
+    def create_rating(self):
+        if not hasattr(self, 'csrf_token'):
+            print("CSRF token not found. Skipping create_rating task.")
+            return
+
+        dataset_id = 1
+        data = {
+            "csrf_token": self.csrf_token,  # Usar el token CSRF almacenado
+            "value": "5",
+            "comment": "Great dataset!"
+        }
+        response = self.client.post(f"/datasets/{dataset_id}/create/rating", data=data)
+        if response.status_code != 200:
+            print(f"Failed to create rating: {response.status_code}")
+        else:
+            print("Rating created successfully")
 
     @task
     def dataset(self):
@@ -41,9 +66,8 @@ class DatasetBehavior(TaskSet):
         else:
             print("Change anonymize (sync) successful")
 
-
-class DatasetUser(HttpUser):
-    tasks = [DatasetBehavior]
+class CombinedUser(HttpUser):
+    tasks = [CombinedBehavior]
     min_wait = 5000
     max_wait = 9000
     host = get_host_for_locust_testing()
