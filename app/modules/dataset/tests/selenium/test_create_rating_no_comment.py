c01a101b6d71058686d0c9d5b1c7f35eaa0d2568
Adrcabmar <adriancabellomartin2003@gmail.com>
2024-12-16 22:14:23 +0100

    bug: tests rating

--- app/modules/dataset/tests/selenium/test_create_rating_no_comment.py
+++ app/modules/dataset/tests/selenium/test_create_rating_no_comment.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 
-class TestCreateratingnocomment():
+class TestNocomment():
     def setup_method(self, method):
         self.driver = webdriver.Chrome()
         self.vars = {}
@@ -11,13 +11,12 @@ class TestCreateratingnocomment():
     def teardown_method(self, method):
         self.driver.quit()
 
-    def test_createratingnocomment(self):
-        self.driver.get("http://192.168.159.127:5000/")
-        self.driver.set_window_size(1050, 699)
-        self.driver.find_element(By.LINK_TEXT, "Ratings").click()
+    def test_nocomment(self):
+        self.driver.get("http://192.168.1.31:5000/")
+        self.driver.set_window_size(1050, 625)
+        self.driver.find_element(By.CSS_SELECTOR, ".nav-link:nth-child(1)").click()
         self.driver.find_element(By.ID, "email").click()
         self.driver.find_element(By.ID, "email").send_keys("user1@example.com")
-        self.driver.find_element(By.CSS_SELECTOR, ".row:nth-child(3) > .col-md-6").click()
         self.driver.find_element(By.ID, "password").click()
         self.driver.find_element(By.ID, "password").send_keys("1234")
         self.driver.find_element(By.ID, "submit").click()
