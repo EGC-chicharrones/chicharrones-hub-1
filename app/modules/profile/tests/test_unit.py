0f6cd0841b81aecb5f64e00f8a1bbd89eab29cd5
María Carrera <carreratalaveron@gmail.com>
2024-12-17 11:36:43 +0100

    test: add unit test for register developer edit profile
    Unit tests (positive and negative) for the edit profile functionality in register developer. CLoses #109

--- app/modules/profile/tests/test_unit.py
+++ app/modules/profile/tests/test_unit.py
@@ -1,9 +1,11 @@
 import pytest
 
 from app import db
+from app.modules.auth.repositories import UserRepository
 from app.modules.conftest import login, logout
 from app.modules.auth.models import User
 from app.modules.profile.models import UserProfile
+from app.modules.profile.services import UserProfileService
 
 
 @pytest.fixture(scope="module")
@@ -48,3 +50,47 @@ def test_get_view_user_profile_does_not_exist(test_client):
 
     response = test_client.get("/profile/123456789")
     assert response.status_code == 404, "The profile editing page was accesed, which should not happen."
+
+
+def test_edit_profile_become_developer_success(test_client):
+    """
+    Tests the successful transition of a normal user to a developer.
+    """
+    login_response = login(test_client, "user@example.com", "test1234")
+    assert login_response.status_code == 200, "Login was successful."
+
+    data = {
+        "name": "Test1",
+        "surname": "Foo1",
+        "orcid": "",
+        "affiliation": "",
+        "is_developer": True,
+        "github_username": "dummy_username"
+    }
+    user_id = UserRepository().get_by_email("user@example.com").id
+
+    UserProfileService().update_profile(user_id, **data)
+
+    assert UserRepository().get_by_id(user_id).is_developer
+    assert UserRepository().get_by_id(user_id).github_username == "dummy_username"
+
+
+def test_edit_profile_become_developer_no_github_username(test_client):
+    """
+    Tests the transition of a normal user to a developer without a GitHub username.
+    """
+    login_response = login(test_client, "user@example.com", "test1234")
+    assert login_response.status_code == 200, "Login was successful."
+
+    data = {
+        "name": "Test1",
+        "surname": "Foo1",
+        "orcid": "",
+        "affiliation": "",
+        "is_developer": True,
+        "github_username": ""
+    }
+    user_id = UserRepository().get_by_email("user@example.com").id
+
+    with pytest.raises(ValueError, match="Developer must have a GitHub username."):
+        UserProfileService().update_profile(user_id, **data)
