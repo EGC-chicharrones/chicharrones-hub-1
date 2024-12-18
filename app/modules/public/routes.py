c2b8b7b7bff6d68135255895fd3ebaf8efcd4ca5
Andrés Pizzano Cerrillos <andpizcer@alum.us.es>
2024-12-18 09:39:52 +0100

    feat: add top 3 datasets by rating
    Modifications of public routes.py. refs #131

--- app/modules/public/routes.py
+++ app/modules/public/routes.py
@@ -5,6 +5,8 @@ from flask import render_template
 from app.modules.featuremodel.services import FeatureModelService
 from app.modules.public import public_bp
 from app.modules.dataset.services import DataSetService
+from app import db
+from app.modules.dataset.models import DSMetaData, DataSet
 
 logger = logging.getLogger(__name__)
 
@@ -36,4 +38,37 @@ def index():
         total_feature_model_downloads=total_feature_model_downloads,
         total_dataset_views=total_dataset_views,
         total_feature_model_views=total_feature_model_views
+    )
+
+
+@public_bp.route('/top_rating', methods=['GET'])
+def top_rating():
+    logger.info("Access index")
+    dataset_service = DataSetService()
+    feature_model_service = FeatureModelService()
+
+    # Statistics: total datasets and feature models
+    datasets_counter = dataset_service.count_synchronized_datasets()
+    feature_models_counter = feature_model_service.count_feature_models()
+
+    # Statistics: total downloads
+    total_dataset_downloads = dataset_service.total_dataset_downloads()
+    total_feature_model_downloads = feature_model_service.total_feature_model_downloads()
+
+    # Statistics: total views
+    total_dataset_views = dataset_service.total_dataset_views()
+    total_feature_model_views = feature_model_service.total_feature_model_views()
+
+    datasets = db.session.query(DataSet).join(DSMetaData).filter(DSMetaData.dataset_doi.isnot(None))
+    datasets = datasets.order_by(-DSMetaData.rating_avg).limit(3)
+    return render_template(
+        "public/index.html",
+        datasets=datasets,
+        datasets_counter=datasets_counter,
+        feature_models_counter=feature_models_counter,
+        total_dataset_downloads=total_dataset_downloads,
+        total_feature_model_downloads=total_feature_model_downloads,
+        total_dataset_views=total_dataset_views,
+        total_feature_model_views=total_feature_model_views,
+        rating=True
     )
